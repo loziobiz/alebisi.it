5ebfa3b1011850e93db2283550d246e53a856385
Alessandro Bisi <alessandro.bisi@keyformat.com>
2025-11-08 18:00:08 +0100

    first version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://alebisi.it',
-  integrations: [react()],
+  output: 'server',
+  adapter: cloudflare(),
 });
 
